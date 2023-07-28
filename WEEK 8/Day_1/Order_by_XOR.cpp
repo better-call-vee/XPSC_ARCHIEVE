9b010dcdc93df3c516daae6d65473373a56fba30
Faiyaz Tanvee <faiyaztanvee009@gmail.com>
2023-07-29 03:24:16 +0600

    29/07/2023

--- WEEK 8/Day_1/Order_by_XOR.cpp
+++ WEEK 8/Day_1/Order_by_XOR.cpp
@@ -1,4 +1,16 @@
-// https://www.codechef.com/problems/XOR_ORDER
+/*
+You need to find a number X such that A XOR X < B XOR X < C XOR X. If there is no such number,
+then you should print -1.
+
+3
+1 2 3
+3 1 2
+3 2 1
+Output:
+4
+-1
+3
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -9,7 +21,7 @@ typedef long long ll;
     cin.tie(nullptr);                 \
     cout.tie(nullptr);
 
-int main()
+int32_t main()
 {
     fast;
 
@@ -21,23 +33,25 @@ int main()
         int A, B, C;
         cin >> A >> B >> C;
 
-        if (A < B && A < C && B < C)
+        if ((A > B) && (B > C))
         {
-            if (C % 2 == 0 && A % 2 != 0)
-                cout << 1 << "\n";
-
-            else if (A % 2 == 0)
-                cout << 1 << "\n";
-
-            else if (C % 2 != 0 && A % 2 != 0)
-                cout << C + 1 << "\n";
+            int X = A | B | C; // bitwise OR operation. here if the A is biggest number, then
+            // we are going to take a big number because, A's XOR value has to be the least.
+            // suppose 5 4 2
+            // the OR of 5 4 2 will be 1 1 1(7). Now if we XOR each of 5 4 2 with 111, we
+            // get 10, 11, 101. so it meets the condition perfectly. what is the logic behind this?
+            // If we XOR a bigger value with a big value, we get a small output. And here if we
+            // OR, we get at least one in every bit because every number is distinct and thus we get
+            // the highest number to do XOR. and thus A is being lessened.
+            cout << X << "\n";
         }
 
-        else if (A > B && A > C && B > C)
-            cout << A << "\n";
+        else if ((A < B) && (B < C))
+            cout << 0 << "\n"; // if the A, B, C are ascendingly sorted, we just simply take 0 because
+        // it's already sorted and XOR with 0 of a number gives the number.
 
         else
-            cout << -1 << "\n";
+            cout << -1 << "\n"; // else -1.
     }
 
     return 0;
